78c7f5bf456017acea4bec12db33b070e0cd200a
loveheenavasu <trivediheenalove@gmail.com>
2023-02-06 16:53:58 +0530

    created models

--- zestgeek_hrm/inventory/models.py
+++ zestgeek_hrm/inventory/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from zestgeek_hrm.user_management.models import CustomUser
+import uuid
+
 
 # Create your models here.
+class Inventory(models.Model):
+    """
+    Items inventory model
+    """
+    inventory_id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    user = models.OneToOneField(CustomUser, on_delete=models.DO_NOTHING)
+    item = models.CharField(max_length=100)
+    item_number = models.CharField(max_length=100)
+    remarks = models.CharField(max_length=100, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- zestgeek_hrm/leaves/models.py
+++ zestgeek_hrm/leaves/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+import uuid
+from zestgeek_hrm.user_management.models import Profile, Department
+# from user_management.models import Department, Profile
 # Create your models here.
+
+
+class Leaves(models.Model):
+    leave_id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    dept_name = models.ForeignKey(Department, on_delete=models.CASCADE)
+    TYPE_CHOICE = (
+        ("FULLDAY", "FULLDAY"), ("HALFDDAY", "HALFDAY"), ("SHORTLEAVE", "SHORTLEAVE")
+    )
+    leave_type = models.CharField(max_length=10, choices=TYPE_CHOICE, default="FULLDAY")
+    STATUS_CHOICE = (
+        ("ACCEPTED", "ACCEPTED"), ("PENDING", "PENDING"), ("CANCELLED", "CANCELLED")
+    )
+    status = models.CharField(max_length=12, choices=STATUS_CHOICE, default="PENDING")
+    start_date = models.DatField()
+    end_date = models.DateField()
+    reason = models.CharField(max_length=200)
+    remaining_leaves = models.IntegerField()
+    attachments = models.CharField(max_length=200, null=True, blank=True)
+    comments = models.CharField(max_length=200, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
