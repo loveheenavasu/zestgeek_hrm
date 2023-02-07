c172201b5f7347a9607a44e29507311283644548
sunny <sunny@tempoclaims.com>
2023-02-07 15:48:09 +0530

    required changes in register template

--- zestgeek_hrm/user_management/urls.py
+++ zestgeek_hrm/user_management/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from user_management.views import *
 
 urlpatterns = [
-    path('register', Register.as_view()),
-    path('employee', Employee.as_view())
+    path('register/', Register.as_view()),
+    path('employee/', Employee.as_view())
 
 
 ]
