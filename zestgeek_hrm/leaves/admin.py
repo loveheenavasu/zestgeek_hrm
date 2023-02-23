15ca6ff91e576d12730ed224c33ad96bb2efdfa3
loveheenavasu <trivediheenalove@gmail.com>
2023-02-23 15:46:42 +0530

    added leaves functionality

--- zestgeek_hrm/leaves/admin.py
+++ zestgeek_hrm/leaves/admin.py
@@ -3,13 +3,13 @@ from .models import *
 
 
 class LeaveAdmin(admin.ModelAdmin):
-    list_display = ('user', 'dept_name', 'status', 'start_date', 'end_date', 'start_time', 'end_time', 'reason','remaining_leaves', 'attachments', 'comments', 'approval_by')
+    list_display = ('user', 'dept_name', 'status', 'start_date', 'end_date', 'reason','remaining_leaves', 'attachments', 'comments', 'approval_by', 'days')
 
 
 admin.site.register(Leaves, LeaveAdmin)
 
 class LeavesDetailsAdmin(admin.ModelAdmin):
-    list_display = ('leave', 'leave_type', 'status', 'start_date', 'end_date', 'days')
+    list_display = ('leave', 'leave_type', 'status', 'start_date', 'end_date', 'start_time', 'end_time')
 
 
 admin.site.register(LeavesDetails, LeavesDetailsAdmin)
